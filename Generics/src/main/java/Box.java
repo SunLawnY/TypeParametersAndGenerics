ebe8f0eae89c716e9be9505a08facfd8312377c3
Sunny <Sunnylaw3571@gmail.com>
2024-04-26 11:53:47 +0100

    First three tasks

--- Generics/src/main/java/Box.java
+++ Generics/src/main/java/Box.java
@@ -1,22 +1,36 @@
-public class Box <T>{
-    private T content;
+class Box<T, V> {
+    private T value;
+    V largestValue;
+    private String name;
 
-    public T getContent() {
-        return content;
+
+    public Box(String name, V largestValue) {
+        this.name = name;
+        this.largestValue = largestValue;
     }
 
-    public void setContent(T content) {
-        this.content = content;
+    public void setValue(T value) {
+        this.value = value;
     }
 
-    Box (T content){
-        this.content = content;
-
+    public T getValue() {
+        return value;
     }
 
-    public void castValue(T content){
-        if (content instanceof this.getClass()){
+    public void setLargestValue(V largestValue) {
+        this.largestValue = largestValue;
+    }
 
+    public T castValue(Object otherValue) {
+        try {
+            System.out.println(otherValue);
+            return (T) otherValue;
+        } catch (ClassCastException e) {
+            throw new ClassCastException("Cannot cast value to type T");
         }
+    }
+
+    public boolean hasSameName(Box<?> otherBox) {
+        return this.name.equals(otherBox.name);
     }
 }
--- Generics/src/main/java/Main.java
+++ Generics/src/main/java/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Box<Integer> intBox = new Box<>("Integer Box");
+        intBox.setValue(42);
+
+        Box<String> stringBox = new Box<>("String Box");
+        stringBox.setValue("Hello, World!");
+
+        System.out.println("Integer value: " + intBox.getValue());
+        System.out.println("String value: " + stringBox.getValue());
+
+        // Attempt to cast a different type
+        try {
+            intBox.castValue("Not an Integer");
+        } catch (ClassCastException e) {
+            System.out.println("ClassCastException caught: " + e.getMessage());
+        }
+
+        // Check if boxes have the same name
+        Box<Double> doubleBox = new Box<>("Integer Box");
+        System.out.println("Same name as intBox? " + intBox.hasSameName(doubleBox));
+        System.out.println("Same name as stringBox? " + stringBox.hasSameName(doubleBox));
     }
 }
